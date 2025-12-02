b5bf56182092648adcfa7c2d7f83777eda9dec84
Kayke simao <kayke.py@gmail.com>
2025-12-02 20:23:02 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,7 +24,7 @@ public class ChessMatch {
         return mat;
     }
 
-    public ChessPiece perfomChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
