d2a26f3913c56ce1cf24309988a3106d539a8b1a
Kayke simao <kayke.py@gmail.com>
2025-12-02 20:22:22 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,7 +27,7 @@ public class Program {
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             } catch (InputMismatchException | ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
